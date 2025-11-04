450c3b0b784280f1a641250260331d06d2ddbaec
N-Yoshida510 <y.naoya735@gmail.com>
2025-11-05 04:26:43 +0900

    Add files via upload

--- kadai_018/Kato_Chapter18.java
+++ kadai_018/Kato_Chapter18.java
@@ -1,15 +1,9 @@
 package kadai_018;
 
 abstract public class Kato_Chapter18 {
-    public String familyName;
+    public String familyName = "加藤";
     public String givenName;
-    public String address;
-
-    // 共通コンストラクタ（苗字と住所は全員同じ）
-    public Kato_Chapter18() {
-        this.familyName = "加藤";
-        this.address = "東京都中野区";
-    }
+    public String address = "東京都中野区〇×";
 
     public void commonIntroduce() {
         System.out.println("私の名前は" + familyName + givenName + "です");
